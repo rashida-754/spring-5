de3b4c66fe3feb31b3d533f9c6adb8db4e07e8ca
rashida.jahan <rashidajahan754@gmail.com>
2021-12-16 15:38:20 +0530

    baseBallCoach will have instance of concrete class based on users choice

--- src/com/spring/springdemo/MyApp.java
+++ src/com/spring/springdemo/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		//create the object
-		BaseBallCoach baseBallCoach = new BaseBallCoach();
+		Coach baseBallCoach = new BaseBallCoach();
 		
 		//use the object	
 		System.out.println(baseBallCoach.getDailyWorkOut());	
